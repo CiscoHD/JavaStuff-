399a1f59cd4ce49b67b18f1ab998c6752c7cb546
CiscoHD <323331535@alumno.enp.unam.mx>
2025-01-03 05:01:24 -0600

    Avance de Proyecto 2 (Conversion str -> int)

---
+++ Practica2/CharToInt.java
@@ -0,0 +1,11 @@
+public class CharToInt
+{
+    public static void main(String[] args)
+    {
+        String str = "01";
+
+        int x = Integer.parseInt(str);
+        System.out.println(x);
+    }
+
+}
--- Practica2/Fechas.java
+++ Practica2/Fechas.java
@@ -2,21 +2,28 @@ public class Fechas
 {
     public static void main(String[] args)
     {
+        String[] meses = {"enero", "febrero", "marzo",
+                          "abril", "mayo", "junio",
+                          "julio", "agosto", "septiembre",
+                          "octubre", "noviembre", "diciembre"};
+
         for(int i = 0; i < args.length; i++)
         {
-
             System.out.println(args[i].charAt(2));
-            
+
             // Identificación de formatos mediante el tercer caracter de la fecha
             if(args[i].charAt(2) == '/')
             {
                 System.out.println("Formato 1, " + args[i].indexOf("/"));
                 String[] formatoUno = args[i].split("/");
+
                 System.out.println("Extensión " + formatoUno.length);
-                for(int j = 0; j < formatoUno.length; j++)
-                {
-                    System.out.println(formatoUno[j] + ", " + j);
-                }
+
+                int dia = Integer.parseInt(formatoUno[0]);
+                System.out.println("El día del argumento " + i + " " + dia);
+
+                int mes = Integer.parseInt(formatoUno[1]);
+                System.out.println("El mes del argumento " + i + " " + meses[mes]);
             }
             else if(args[i].charAt(2) == ' ')
             {
@@ -33,6 +40,8 @@ public class Fechas
                 System.out.println("Formato no reconocido");
             }
         }
-
     }
 }
+
+
+
