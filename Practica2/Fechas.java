abf0943dd662e41d7e29d45db2f9ca20f51f47a3
CiscoHD <323331535@alumno.enp.unam.mx>
2025-01-02 20:24:39 -0600

    Actualización de la  Práctica 2 (Fechas)

--- Practica2/Fechas.java
+++ Practica2/Fechas.java
@@ -6,15 +6,27 @@ public class Fechas
         {
 
             System.out.println(args[i].charAt(2));
-
+            
             // Identificación de formatos mediante el tercer caracter de la fecha
             if(args[i].charAt(2) == '/')
             {
-                System.out.println("Formato 1");
+                System.out.println("Formato 1, " + args[i].indexOf("/"));
+                String[] formatoUno = args[i].split("/");
+                System.out.println("Extensión " + formatoUno.length);
+                for(int j = 0; j < formatoUno.length; j++)
+                {
+                    System.out.println(formatoUno[j] + ", " + j);
+                }
             }
             else if(args[i].charAt(2) == ' ')
             {
                 System.out.println("Formato 2");
+                String[] formatoDos = args[i].split(" ");
+                System.out.println("Extensión" + formatoDos.length);
+                for(int j = 0; j < formatoDos.length; j++)
+                {
+                    System.out.println(formatoDos[j] + ", " + j);
+                }
             }
             else
             {
